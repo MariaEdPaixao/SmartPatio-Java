c25e29e3cff5837fcf5524bc84579135440e4cb7
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-09-22 21:41:21 -0300

    feat: projeto inicial

---
+++ src/main/java/fiap/com/br/SmartPatio/SmartPatioApplication.java
@@ -0,0 +1,13 @@
+package fiap.com.br.SmartPatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPatioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPatioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/com/br/SmartPatio/SmartPatioApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.com.br.SmartPatio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPatioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
