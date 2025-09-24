d28c666c671c5c16e2a30dd208922165b85c82bb
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-09-23 22:46:48 -0300

    feat: config security

--- src/main/java/fiap/com/br/SmartPatio/datasource/repository/UserRepository.java
+++ src/main/java/fiap/com/br/SmartPatio/datasource/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package fiap.com.br.SmartPatio.datasource.repository;
 
-public interface UserRepository {
+import fiap.com.br.SmartPatio.domainmodel.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByEmail(String email);
 }
--- src/main/java/fiap/com/br/SmartPatio/service/AuthorizationService.java
+++ src/main/java/fiap/com/br/SmartPatio/service/AuthorizationService.java
@@ -1,4 +1,21 @@
 package fiap.com.br.SmartPatio.service;
 
-public class AuthorizationService {
+import fiap.com.br.SmartPatio.datasource.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public AuthorizationService(final UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username);
+    }
 }
