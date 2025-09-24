3db354cde79252d1ba769ae3b030be42effb3bb6
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-09-23 22:47:50 -0300

    feat: pags de login e cadastro com css

--- src/main/java/fiap/com/br/SmartPatio/controller/UserController.java
+++ src/main/java/fiap/com/br/SmartPatio/controller/UserController.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class UserController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
     }
 }
