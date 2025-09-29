4376d345e437930965d8af74b143272f09fc886c
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-09-29 10:59:11 -0300

    feat: migration de comentarios

---
+++ src/main/resources/db/migration/V4__comments_tables.sql
@@ -0,0 +1,5 @@
+COMMENT ON TABLE filial IS 'Tabela que guarda informações das filiais da Mottu';
+COMMENT ON TABLE moto IS 'Tabela que guarda as motos das filiais da Mottu';
+COMMENT ON TABLE carrapato IS 'Tabela que guarda os carrapatos das filiais';
+COMMENT ON COLUMN usuario.role IS 'Pode ser GESTOR ou FUNCIONARIO';
+COMMENT ON TABLE historico_moto_filial IS 'Histórico de entrada e saída de motos nos pátios. Entrada cria inserção e saída atualzia a inserção.';
