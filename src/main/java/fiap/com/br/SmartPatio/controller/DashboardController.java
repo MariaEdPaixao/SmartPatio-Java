9531f463efd2aae26c37285b2f60474980c6aaf2
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-09-28 22:27:59 -0300

    feat: dashboard do historico da filial

--- src/main/java/fiap/com/br/SmartPatio/controller/DashboardController.java
+++ src/main/java/fiap/com/br/SmartPatio/controller/DashboardController.java
@@ -1,13 +1,47 @@
 package fiap.com.br.SmartPatio.controller;
 
+import fiap.com.br.SmartPatio.domainmodel.Carrapato;
+import fiap.com.br.SmartPatio.domainmodel.HistoricMotorcycleFilial;
+import fiap.com.br.SmartPatio.domainmodel.User;
+import fiap.com.br.SmartPatio.domainmodel.enums.HistoricMotorcycleStatus;
+import fiap.com.br.SmartPatio.service.CarrapatoServiceImpl;
+import fiap.com.br.SmartPatio.service.HistoricMotorcycleFilialServiceImpl;
+import fiap.com.br.SmartPatio.service.UserServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+import java.util.List;
+
 @Controller
-public class HomeController {
+public class DashboardController {
+
+    private final HistoricMotorcycleFilialServiceImpl historicoService;
+    private final CarrapatoServiceImpl carrapatoService;
+    private final UserServiceImpl userService;
+
+    public DashboardController(HistoricMotorcycleFilialServiceImpl historicoService,
+                               CarrapatoServiceImpl carrapatoService, UserServiceImpl userService) {
+        this.historicoService = historicoService;
+        this.carrapatoService = carrapatoService;
+        this.userService = userService;
+    }
+
 
     @GetMapping("/")
-    public String home() {
+    public String dashboard(Model model, Principal principal) {
+        User user = userService.findByEmail(principal.getName())
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        List<HistoricMotorcycleFilial> ativos = historicoService.findByStatusAndFilial(HistoricMotorcycleStatus.ATIVA, user.getFilial());
+        List<HistoricMotorcycleFilial> finalizadas = historicoService.findByStatusAndFilial(HistoricMotorcycleStatus.FINALIZADA, user.getFilial());
+        List<Carrapato> carrapatos = carrapatoService.findTop5ByOrderByNivelBateriaAsc();
+
+        model.addAttribute("user", user);
+        model.addAttribute("ativos", ativos);
+        model.addAttribute("finalizadas", finalizadas);
+        model.addAttribute("carrapatos", carrapatos);
+
         return "index";
     }
 }
--- src/main/java/fiap/com/br/SmartPatio/service/HistoricMotorcycleFilialService.java
+++ src/main/java/fiap/com/br/SmartPatio/service/HistoricMotorcycleFilialService.java
@@ -1,4 +1,11 @@
 package fiap.com.br.SmartPatio.service;
 
-public class HistoricMotorcycleFilialService {
+import fiap.com.br.SmartPatio.domainmodel.Filial;
+import fiap.com.br.SmartPatio.domainmodel.HistoricMotorcycleFilial;
+import fiap.com.br.SmartPatio.domainmodel.enums.HistoricMotorcycleStatus;
+
+import java.util.List;
+
+public interface HistoricMotorcycleFilialService {
+    List<HistoricMotorcycleFilial> findByStatusAndFilial(HistoricMotorcycleStatus status, Filial filial);
 }
